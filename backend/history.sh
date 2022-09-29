3d99e438d164d9a4341ca114b620b9fe8f7a9304
Jhordy Gavinchu <dev@jhordyess.com>
2022-09-29 19:13:30 -0400

    Clean code

--- backend/history.sh
+++ backend/history.sh
@@ -1,9 +1,8 @@
 #? Commands history
 sudo npm i npm -g
 # npm init -y
-npm i cors express morgan 
+npm i cors express morgan @prisma/client
 npm i -D nodemon prisma
-npm i @prisma/client
 # npx prisma init
 npx prisma generate
 npx prisma db push
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
-import routerProduct from "./routes/product.routes.js";
-import routerCategory from "./routes/category.routes.js";
-import routerBrand from "./routes/brand.routes.js";
+import productRouter from "./routes/product.routes.js";
+import categoryRouter from "./routes/category.routes.js";
+import brandRouter from "./routes/brand.routes.js";
 
 const app = express();
 const port = process.env.PORT || 4062;
@@ -14,9 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //?
 app.use(cors({ origin: "*" }));
 
-app.use("/product", routerProduct);
-app.use("/category", routerCategory);
-app.use("/brand", routerBrand);
+app.use("/product", productRouter);
+app.use("/category", categoryRouter);
+app.use("/brand", brandRouter);
 
 app.listen(port, () => {
   console.log("Server initialized");
