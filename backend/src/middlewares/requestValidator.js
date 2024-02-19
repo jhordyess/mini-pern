b3baca1108e30bf1ef643ef247484567e93fe24c
Jhordy Gavinchu <dev@jhordyess.com>
2024-02-19 16:03:59 +0000

    Added express-validator

---
+++ backend/src/middlewares/requestValidator.js
@@ -0,0 +1,17 @@
+import { validationResult } from 'express-validator'
+import { HttpError } from '../utils/error'
+
+export const checkRequest = (req, _, next) => {
+  try {
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      const messages = result.array().map(error => error.msg)
+      throw new HttpError(messages.join(', '), 400)
+    }
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const withValidation = (validation, handler) => [validation, checkRequest, handler]
